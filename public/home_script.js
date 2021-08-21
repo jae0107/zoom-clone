cc4b229be686e3af57c794767a6a9752e6f1eed6
jae0107 <ghjgjh0107@gmail.com>
2021-08-22 00:00:52 +0930

    fix rejoin room

--- public/home_script.js
+++ public/home_script.js
@@ -6,6 +6,8 @@ const gotoRoom = () => {
     window.location.href = '/room';
 }
 
-if(window.location.href !== document.referrer && document.referrer !== ""){
-    $('.rejoin').append("<button onclick='rejoin()'>Rejoin the Room</button>");
+if(window.location.href !== document.referrer 
+    && document.referrer !== "" 
+    && document.referrer.substr(0, ("https://" + document.domain).length) === ("https://" + document.domain)){
+        $('.rejoin').append("<button onclick='rejoin()'>Rejoin the Room</button>");
 }
